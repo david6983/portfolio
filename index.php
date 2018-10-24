e83cc74219ade17d3c961acbf58f559e45f139a3
david <dav6983@gmail.com>
2018-10-24 15:48:25 +0200

    working on the camelot heel img but issue on css to solve

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>2KEY</title>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" type="text/css" href="src/css/index.css">
-    <script type="text/javascript" src="src/js/collapseMenuBar.js"></script>
-    <script type="text/javascript" src="src/js/sidebar.js"></script>  
+    <script src="src/js/collapseMenuBar.js"></script>
+    <script src="src/js/sidebar.js"></script>  
 </head>
 <body>
     <header>
@@ -16,7 +16,7 @@
         <nav class="w3-bar w3-blue">
             <!-- collapse sidebar button -->
             <button class="w3-button w3-left w3-xlarge w3-hide-large" onclick="openSideBar()">
-                <img src="assets/icons/playlist.png" id="playlistIcon" alt="acces to playlists"></img>
+                <img src="assets/icons/playlist.png" id="playlistIcon" alt="acces to playlists">
             </button>
             <!-- Logo -->
             <div class="w3-bar-item w3-left mainLogo">2KEY</div>
@@ -30,15 +30,17 @@
                 <a href="#" class="w3-bar-item w3-button w3-mobile navMiddleButton">Settings</a>
                 <a href="#" class="w3-bar-item w3-button w3-mobile navMiddleButton">Tutorial</a>
                 <!-- search and user profile icons -->
-                <a href="#" class="w3-bar-item w3-button w3-mobile w3-right"><img src="assets/icons/user.png" class="navleftIcon" alt="userProfile"></img></a>
-                <a href="#" class="w3-bar-item w3-button w3-mobile w3-right"><img src="assets/icons/search.png" class="navleftIcon" alt="toggle search bar"></img></a>
+                <a href="#" class="w3-bar-item w3-button w3-mobile w3-right"><img src="assets/icons/user.png" class="navleftIcon" alt="userProfile"></a>
+                <a href="#" class="w3-bar-item w3-button w3-mobile w3-right"><img src="assets/icons/search.png" class="navleftIcon" alt="toggle search bar"></a>
             </div>                       
         </nav>
         
     </header>
     <!-- sidebar on the left -->
     <div id="sidebar" class="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left">
-        <img id="camalotImg" src="assets/img/camalote-wheel-logo.png" alt="camalote wheel of keys"></img>
+        <img src="assets/img/camalote-wheel-logo.png" alt="camalote wheel of keys" style="width: 250px;
+            margin-left: 19%;
+            margin-top: 20px;">
     </div>
     <div>
         <div class="w3-card w3-margin">
