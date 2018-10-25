10757450e236e6be1172397130751e616abd5bba
david <dav6983@gmail.com>
2018-10-25 19:28:15 +0200

    started a sign in form

---
+++ src/pages/signIn.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>2KEY</title>
+    <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+    <div class="w3-container">
+        <h1>2KEY</h1>
+    </div>
+    <div class="w3-container">
+        <h2>Connexion</h2>
+    </div>
+
+    <form method="POST" action="" class="w3-container">
+        <p>
+            <label>Login</label>
+            <input class="w3-input" type="text" required>
+        </p>
+        <p>
+            <label>Password</label>
+            <input class="w3-input" type="password" required>
+        </p>
+        <p>
+            <input type="submit" value="Sign in">
+        </p>
+    </form>
+
+</body>
+</html>
