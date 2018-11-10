13c50a09dd98376cb41ce71fa2bf2a1bc5e4f62b
david <dav6983@gmail.com>
2018-11-10 20:38:25 +0100

    refactoring classesManagers

--- src/phpClassesManagers/playlistsManager.php
+++ src/phpClassesManagers/playlistsManager.php
@@ -1,3 +1,45 @@
 <?php   
-    
+    class PlaylistsManager {
+        private $_host; 
+        private $_db; 
+        private $_user; 
+        private $_pass; 
+        private $_dbh; 
+
+        public function __construct($host,$db,$user,$pass){
+            $this->_host = $host;
+            $this->_db = $db;
+            $this->_user = $user;
+            $this->_pass = $pass;
+        }
+
+        /**
+         * se connecte a la bdd a partir des attributs
+         */
+        public function connect(){
+            try {
+                $this->_dbh = new PDO('mysql:host='.$this->_host.';dbname='.$this->_db,$this->_user,$this->_pass);
+            }
+            catch(PDOException $e)
+            {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        }
+
+        public function addPlaylist(){
+
+        }
+
+        public function deletePlaylist(){
+
+        }
+
+        public function getPlaylist(){
+        
+        }
+
+        public function updatePlaylist(){
+
+        }
+    }
 ?>
