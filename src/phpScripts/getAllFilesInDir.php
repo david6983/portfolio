4b0b170d021cc53e11de8ecea89db8490af65c67
david <dav6983@gmail.com>
2018-11-10 22:34:04 +0100

    added getAllFilesInDir.php

---
+++ src/phpScripts/getAllFilesInDir.php
@@ -0,0 +1,13 @@
+<?php 
+    function getAllFilesInDir($dir){
+        $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+        $files = array(); 
+        foreach ($rii as $file) {
+            if ($file->isDir()){ 
+                continue;
+            }
+            $files[] = utf8_encode($file->getPathname()); 
+        }
+        return $files;
+    }
+?>
