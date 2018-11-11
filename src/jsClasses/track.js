6b99a7368e9ecd5dc8b4ecbcc74231fc62ac5478
david <dav6983@gmail.com>
2018-11-11 15:19:13 +0100

    track.js updated

--- src/jsClasses/track.js
+++ src/jsClasses/track.js
@@ -6,12 +6,13 @@ class Track {
         this._genre = genre;
         this._localpath = localpath;
         this._bpm = null;
-        this._lenght = "";
+        this._lenght = 0;
         this._key = "";
     }
     analyzeTrack(precision){
-        this._bpm = this.findBPM(precision);
-        this._key = this.findKey();
+        //this._bpm = this.findBPM(precision);
+        //this._key = this.findKey();
+        this.pathFromLocalToServer();
         this._lenght = this.findLenght();
     }
     findBPM(precision){
@@ -93,34 +94,62 @@ class Track {
     }
       
     findKey(){
-        let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        let analyser = audioCtx.createAnalyser();
-        let src = audioCtx.createMediaStreamSource(this.pathFromLocalToServer(localpath));
-        src.connect(analyser);
-        analyser.fftSize = 2048;
-        let bufferLength = analyser.frequencyBinCount;
-        let dataArray = new Uint8Array(bufferLength);
-        analyser.getByteDomainData(dataArray);
-        console.log(dataArray);
-        this._key = dataArray[0];
+        this.requestFreq(this.updateKey,this);
     }
-    findLenght(){
+    updateKey(freq){
+        console.log(freq);
+    }
+    requestFreq(callback,object){
         let audioCtx = new AudioContext();
+        let analyser = audioCtx.createAnalyser();
+        analyser.fftSize = 2048;
         this.pathFromLocalToServer();
         let src = audioCtx.createBufferSource();
-        var audioBuffer;
-        var request = new XMLHttpRequest();
-        request.open("GET",this._localpath,true);
+        let bufferLength = analyser.frequencyBinCount;
+        let dataArray = new Uint8Array(bufferLength);
+        /*
+        console.log(Math.round(dataArray[0]);
+        */
+        var request = getXMLHttpRequest();
         request.responseType = "arraybuffer";
-        request.onload = function(){
-            audioCtx.decodeAudioData(
-                request.response,
-                function(buffer){
-                    audioBuffer = buffer;
-                    this._lenght = audioBuffer.duration;
-                }
-            );
-        }
+        request.onreadystatechange = function () {
+            if(request.readyState === 4 && request.status === 200) {
+                audioCtx.decodeAudioData(
+                    request.response,
+                    function(buffer){
+                        src.connect(analyser);
+                        analyser.getByteTimeDomainData(dataArray);
+                        console.log(dataArray[0]);  
+                    }
+                );
+            }
+        };
+        request.open("GET",this._localpath,true);
+        request.send();
+    }
+    findLenght(){
+        this.requestLenght(this.updateLenght,this);
+    }
+    updateLenght(lenght,object){
+        object.lenght = lenght;
+    }
+    requestLenght(callback,object){
+        let audioCtx = new AudioContext();
+        var audioBuffer;
+        var request = getXMLHttpRequest();
+        request.responseType = "arraybuffer";
+        request.onreadystatechange = function () {
+            if(request.readyState === 4 && request.status === 200) {
+                audioCtx.decodeAudioData(
+                    request.response,
+                    function(buffer){
+                        audioBuffer = buffer;
+                        callback(Math.round(audioBuffer.duration),object);    
+                    }
+                );
+            }
+        };
+        request.open("GET",this._localpath,true);
         request.send();
     }
     pathFromLocalToServer(){
@@ -131,19 +160,42 @@ class Track {
         this.newPath = serverPath.replace(" ","%20");
     }
     set bpm(bpm){
-        this._bpm = bpm;
+        const reg = /[0-9]{3}|[0-9]{2}/g;
+        if(typeof bpm === "number"){
+            if(reg.test(bpm) == true && bpm.toString().length < 4){
+                this._bpm = bpm;
+            }else{
+                console.log("error syntax : expected 2 digits or 3 digits");
+            }
+        }else{
+            console.log("the new BPM is not a number")
+        }
     }
     set key(key){
-        this._key = key;
+        const reg = /[1-9]{1}[A-B]{1}|[1]{1}[0-2]{1}[A-B]{1}/y;
+        if(typeof key === "string" && reg.test(key) == true){
+            this._key = key;
+        }else{
+            console.log("error expected a string like [a number between 1 and 12][A or B] (camelot wheel)");
+        }
     }
     set genre(genre){
-        this._genre = genre;
+        if(typeof genre === "string"){
+            this._genre = genre;
+        }else{
+            console.log("error expected a string");
+        }
     }
     set lenght(lenght){
-        this._lenght = lenght
-    }
+        this._lenght = lenght;
+    }    
     set newPath(localpath){
-        this._localpath = localpath
+        const reg = /[A-Z]:|http:[//][//]/y;
+        if(typeof localpath === "string" && reg.test(localpath) === true){
+            this._localpath = localpath.replace(" ","%20");
+        }else{
+            console.log("error : expected a correct path like D:/ or C:/ ");
+        }    
     }
     get id(){
         return this._id;
@@ -164,9 +216,10 @@ class Track {
         return this._bpm;   
     }    
     get lenght(){
+        console.log("test");
         return this._lenght;
     }
     get key(){
-    return this._key;   
+        return this._key;   
     }
 }
