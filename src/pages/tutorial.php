5ba212dd836f916accb367a0e7bf84b9bc987f7a
david <dav6983@gmail.com>
2018-11-12 11:31:23 +0100

    tutorial page ok

--- src/pages/tutorial.php
+++ src/pages/tutorial.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="overflow: auto;">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -48,7 +48,7 @@
                 <div class="w3-container w3-white">
                     <p>
                         <label class="w3-tooltip"><b>What is 2KEY ?</b> 2KEY is a website project to create a music's library manager.</label><br/>
-                        <label class="w3-tooltip"><b>Who can use 2KEY ?</b> Everybody can use 2KEY, we have made a simple system. </label><br/>
+                        <label class="w3-tooltip"><b>Who can use 2KEY ?</b> 2KEY is especially for Musician and DJ, but the concept can be understand by everyone.  </label><br/>
                     </p>
                 </div> 
             </div>
@@ -62,17 +62,17 @@
                 <div class="w3-container w3-white">
                     <p>
                         <b>Toolbar</b><br/>
-                        <label class="w3-tooltip"><b>Collections :</b> In this page you can access to your collection of music.</label><br/>
-                        <label class="w3-tooltip"><b>Tags :</b> In this page you can manage the Tags of your music.</label><br/>
-                        <label class="w3-tooltip"><b>Settings :</b> In this page you can access to your settings of your account.</label><br/>
-                        <label class="w3-tooltip"><b>Tutorial :</b> In this page you can access to your collection of music.</label><br/>
+                        <label class="w3-tooltip"><b>Collections :</b> In "Collections" you can access to your collection of music.</label><br/>
+                        <label class="w3-tooltip"><b>Tags :</b> By clicking on "Tags" you can edit/change Tags on your music. </label><br/>
+                        <label class="w3-tooltip"><b>Settings :</b> In "Settings" you can access and change the settings of your session.</label><br/>
+                        <label class="w3-tooltip"><b>Tutorial :</b> You are already inside "Tutorial" and I am sure you know why.</label><br/>
                     </p>
 
                     <p>
                         <b>Collection</b><br/>
-                        <label class="w3-tooltip"><b>Analyse your track :</b> This button give you informations about your tracks (key, bpm ...)</label><br/>
+                        <label class="w3-tooltip"><b>Analyse your track :</b> This button give you informations about your tracks like the key, bpm and lenght</label><br/>
                         <label class="w3-tooltip"><b>New collection :</b> This button is for create a new collection/playlist of musics.</label><br/>
-                        <label class="w3-tooltip"><b>View all :</b> This button will show you all your collection.</label><br/>
+                        <label class="w3-tooltip"><b>View all :</b> This button will show you all your musics.</label><br/>
                         <label class="w3-tooltip"><b>Export/Import :</b> You can export/import .json files.</label><br/>
                     </p>
 
@@ -93,8 +93,14 @@
                 </div>
                 <div class="w3-container w3-white">
                     <p>
-                        <label class="w3-tooltip"> </label><br/>
-
+                        <label class="w3-tooltip">-Click on your Wamp Application </label><br/>
+                        <label class="w3-tooltip">-Click on "Your VirtualHost" </label><br/>
+                        <label class="w3-tooltip">-Click on "VirtualHost Management" </label><br/>
+                        <img id="VH1" src="../../assets/img/VH1.jpg" alt="Picture of the first step for the VirtualHost"><br/>
+                        <label class="w3-tooltip">-Write the name of your virtualhost "2KEY" and create a folder for the path here "D:\2key" </label><br/>
+                        <label class="w3-tooltip">-Do the same manipulation for your local music, writing "Musics" and the path of your local music</label><br/>
+                        <img id="VH2" src="../../assets/img/VH2.jpg" alt="Picture of the second step for the VirtualHost"><br/>
+                        <label class="w3-tooltip">-Click on "Restart All Services" in your Wamp Application </label><br/>
                     </p>
                 </div> 
             </div>
@@ -111,8 +117,10 @@
                         <label class="w3-tooltip">-Click on "Apache" </label><br/>
                         <label class="w3-tooltip">-Click on "Apache Modules" </label><br/>
                         <label class="w3-tooltip">-Enable "headers_module" </label><br/>
+                        <img id="Tuto1" src="../../assets/img/tuto1.png" alt="Picture of the first step"><br/>
                         <label class="w3-tooltip">-Then open "httpd.conf" in notepad </label><br/>
                         <label class="w3-tooltip">- Add this line : <IfModule mod_headers.c> Header set Access-Control-Allow-Origin: *</IfModule> </label><br/>
+                        <img id="Tuto2" src="../../assets/img/tuto2.png" alt="Picture of the second step"><br/>
 
 
                     </p>
